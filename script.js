19c401311f4accfe0a57ca467a3d31b5ca4de14c
k-cicek <kevserrcicek@gmail.com>
2021-11-23 15:25:39 +0300

    Updated

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ const form = document.querySelector("form");
 const input = document.querySelector("#txtTaskName");
 const btnDeleteAll = document.querySelector("#btnDeleteAll");
 const taskList = document.querySelector("#task-list");
-const items = [
-  "3 Litre Su İç",
-  "Ödevleri Yap",
-  "En Az 3 Saat Kodlama Yap",
-  "Yemek Yap",
-];
+let items;
 
 //load items
 loadItems();
@@ -28,23 +23,61 @@ function eventListeners() {
   btnDeleteAll.addEventListener("click", deleteAllItems);
 }
 
+// Done it
+var list = document.querySelector('ul')
+list.addEventListener('click', function (check) {
+    if (check.target.tagName === 'LI') {
+        check.target.classList.toggle('checked')
+    }
+})
+
 function loadItems() {
+  items = getItemsFromLS();
+
   items.forEach(function (item) {
     createItem(item);
   });
 }
 
+// get item from local storage
+function getItemsFromLS() {
+  if (localStorage.getItem("items") === null) {
+    items = [];
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
+  return items;
+}
+
+// set item to local storage
+function setItemToLS(text) {
+  items = getItemsFromLS();
+  items.push(text);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+// delete item from LS
+function deleteItemFromLS(text) {
+  items = getItemsFromLS();
+  items.forEach(function (item, index) {
+    if (item === text) {
+      items.splice(index, 1);
+    }
+  });
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
 function createItem(text) {
   //create li
   const li = document.createElement("li");
-  li.className = "list-group-item list-group-item-secondary";
+  li.className = "list-group-item list-group-item-secondary pl-5";
   li.appendChild(document.createTextNode(text));
 
   //create a
   const a = document.createElement("a");
   a.classList = "delete-item float-right";
   a.setAttribute("href", "#");
-  a.innerHTML = '<i class="fas fa-times"></i>';
+  a.innerHTML = '<i class="fas fa-times" style="color:gray;"></i>';
 
   //add a to li
   li.appendChild(a);
@@ -61,6 +94,9 @@ function addNewItem(e) {
   //create item
   createItem(input.value);
 
+  // save to LS
+  setItemToLS(input.value);
+
   //clear input
   input.value = "";
 
@@ -74,6 +110,9 @@ function deleteItem(e) {
   if (e.target.className === "fas fa-times") {
     if (confirm("are you sure?")) {
       e.target.parentElement.parentElement.remove();
+
+      // delete item from LS
+      deleteItemFromLS(e.target.parentElement.parentElement.textContent);
     }
   }
 
@@ -83,7 +122,10 @@ function deleteItem(e) {
 // delete All items
 function deleteAllItems(e) {
   if (confirm("are you sure?")) {
-    taskList.innerHTML = "";
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
   }
+  localStorage.clear();
   e.preventDefault();
 }
